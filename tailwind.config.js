df0eecf4bd17d0d071b8afd0a8cc8ece6d524ec2
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-09-05 17:27:10 +0600

    modify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-import {nextui} from "@nextui-org/react";
-
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -7,16 +6,18 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}", // For NextUI components
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      // You can also extend other theme properties here if needed
     },
   },
-  plugins: [nextui()]
+  plugins: [
+    nextui(), // Include NextUI plugin
+  ],
 };
